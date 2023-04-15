304b6f13c3626aff19f1bf67f10aa96072cc34fd
Neil Smith <me+github@neilsmith.fyi>
2023-04-15 02:11:51 -0500

    build hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-portrait-bg": "url('/color-splotch-01.png')",
+        border: "url('/border2.svg')",
+      },
+      colors: {
+        beige: "#E9E8DC",
+        brown: "#211805",
+      },
+      fontFamily: {
+        heading:
+          '"Lalezar", Bahnschrift, "DIN Alternate", "Franklin Gothic Medium", "Nimbus Sans Narrow", sans-serif-condensed, sans-serif',
+      },
+    },
   },
   plugins: [],
 }
